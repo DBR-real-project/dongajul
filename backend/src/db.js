896c61a3329430e1bedca8245219a4d28d30a165
DongYeon <zhco0527@gmail.com>
2026-05-28 09:02:01 +0900

    db 연동 완료

---
+++ backend/src/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('DB 연결 실패:', err);
+  } else {
+    console.log('DB 연결 성공');
+  }
+});
+
+module.exports = connection;
